e34a9036b520a1d28f7d930da4ef25db71c4f457
ktoshiya <k.toshiya.1107@gmail.com>
2021-03-15 16:39:58 +0900

    fix eslintrc

---
+++ eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": ["eslint:recommended", "plugin:prettier/recommended"],
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "trailingComma": "es5"
+      }
+    ],
+    "semi": ["error", "always"],
+    "semi-spacing": ["error", {"after": true, "before": false}],
+    "semi-style": ["error", "last"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error"
+  }
+};
--- src/App.js
+++ src/App.js
@@ -8,28 +8,27 @@ import Home from './screens/Home';
 import Profile from './screens/Profile';
 import SignInOrUp from './screens/SignInOrUp';
 import SignUp from './screens/SignUp';
-
 import Auth from './Auth';
 
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/signin" component={SignInOrUp} />
-                    <Route exact path="/signup" component={SignUp} />
-                    {/* 以下認証のみ */}
-                    <Auth>
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/profile" component={Profile} />
-                            <Route render={() => <p>not found.</p>} />
-                        </Switch>
-                    </Auth>
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/signin" component={SignInOrUp} />
+          <Route exact path="/signup" component={SignUp} />
+          {/* 以下認証のみ */}
+          <Auth>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/profile" component={Profile} />
+              <Route render={() => <p>not found.</p>} />
+            </Switch>
+          </Auth>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
